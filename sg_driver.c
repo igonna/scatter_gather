44b5fcfcb1b103f24c41cbade9ed5daf3b5bec23
Yegor Yudin <northearts at gmail dot com>
2020-10-03 05:43:04 +0300

    driver

--- sg_driver.c
+++ sg_driver.c
@@ -31,43 +31,9 @@
 }SG_Packet_WD;*/
 //
 // Global Data
-typedef struct {
-    uint32_t		magic_v1;
-    SG_Node_ID		locNodeId;
-    SG_Node_ID		remNodeId;
-    SG_Block_ID		blockID;
-    SG_System_OP	operation;
-    SG_SeqNum		sendSeqNo;
-    SG_SeqNum		recvSeqNo;
-    char		indc_data;
-    uint32_t		magic_v2;
-}SG_Packet_ND;
 
-typedef struct {
-    uint32_t		magic_v1;
-    SG_Node_ID		locNodeId;
-    SG_Node_ID		remNodeId;
-    SG_Block_ID		blockID;
-    SG_System_OP	operation;
-    SG_SeqNum		sendSeqNo;
-    SG_SeqNum		recvSeqNo;
-    char		indc_data;
-    SG_Data_Block	*data;
-    uint32_t		magic_v2;
-}SG_Packet_WD;
 //
 // Functions
-
-void sg_strcpy_t(SG_Data_Block *dblk, char *str)
-{
-	for (int i = 0; i < strlen(str) + 1; ++i)
-		(*dblk)[i] = str[i];
-}
-void sg_strcpy_f(char *str, SG_Data_Block *dblk)
-{
-	for (int i = 0; i < SG_BLOCK_SIZE; ++i)
-		str[i] = (*dblk)[i];
-}
 
 //from SG_Packet to char* 
 void sg_pckt_buff(char *packet, void *pckt_wd, size_t sz)
@@ -75,14 +41,12 @@ void sg_pckt_buff(char *packet, void *pckt_wd, size_t sz)
     for (uint16_t step = 0; step < sz; ++step)
             packet[step] = ((char*)pckt_wd)[step];
 }
-void sg_strcpy(char *buff, char *sg_buff)
+void impose_buff(char *buff_to, char *buff_from, int begin, uint32_t end, uint32_t margin)
 {
-    for (uint16_t step = 0; step < SG_BLOCK_SIZE; ++step)
-            if (sg_buff + step)
-                    buff[step] = sg_buff[step];
-            else
-                    break;
+    for (; begin <= end; ++begin)
+        buff_to[margin++] = buff_from[begin];
 }
+
 ////////////////////////////////////////////////////////////////////////////////
 //
 // Function     : serialize_sg_packet
@@ -140,75 +104,50 @@ SG_Packet_Status serialize_sg_packet( SG_Node_ID loc, SG_Node_ID rem, SG_Block_I
 		pack_status = SG_PACKT_RCVSQ_BAD;
 	//
 	char data_indc;
-	if (!data) {
+	if (data) {
 		*plen = SG_DATA_PACKET_SIZE;
-		data_indc = 0;
+		data_indc = 1;
 
 	} else {
 		*plen = SG_BASE_PACKET_SIZE;
-		data_indc = 1;
+		data_indc = 0;
 	}
 
-//	printf("\t-sindc: %d\n", data_indc);
-
-	SG_Packet_WD sg_packet_wd;
-	SG_Packet_ND sg_packet_nd;
+	bzero(packet, SG_DATA_PACKET_SIZE);
+	uint32_t packet_margin = 0;
+	uint32_t magic_num = SG_MAGIC_VALUE;
+	//magic
+	strcpy(packet, (char*)&magic_num);
+	packet_margin += sizeof(uint32_t);
+	//locNodeId
+	impose_buff(packet, (char*)&pack_info.locNodeId, 0, sizeof(SG_Node_ID) - 1, packet_margin);
+	packet_margin += sizeof(SG_Node_ID);
+	//remNodeId
+	impose_buff(packet, (char*)&pack_info.remNodeId, 0, sizeof(SG_Node_ID) - 1, packet_margin);
+	packet_margin += sizeof(SG_Node_ID);
+	//blockID
+	impose_buff(packet, (char*)&pack_info.blockID, 0, sizeof(SG_Block_ID) - 1, packet_margin);
+	packet_margin += sizeof(SG_Block_ID);
+	//operation
+	impose_buff(packet, (char*)&pack_info.operation, 0, sizeof(SG_System_OP) - 1, packet_margin);
+	packet_margin += sizeof(SG_System_OP);
+	//sendSeqNo
+	impose_buff(packet, (char*)&pack_info.sendSeqNo, 0, sizeof(SG_SeqNum) - 1, packet_margin);
+	packet_margin += sizeof(SG_SeqNum);
+	//recvSeqNo	
+	impose_buff(packet, (char*)&pack_info.recvSeqNo, 0, sizeof(SG_SeqNum) - 1, packet_margin);
+	packet_margin += sizeof(SG_SeqNum);
+	//indicator
+	impose_buff(packet, (char*)&data_indc, 0, sizeof(char) - 1, packet_margin);
+	packet_margin += sizeof(char);
+	//data
 	if (data_indc) {
-		sg_packet_wd.magic_v1 = SG_MAGIC_VALUE;
-		sg_packet_wd.locNodeId = pack_info.locNodeId;
-		sg_packet_wd.remNodeId = pack_info.remNodeId;
-		sg_packet_wd.blockID = pack_info.blockID;
-		sg_packet_wd.operation = pack_info.operation;
-		sg_packet_wd.sendSeqNo = pack_info.sendSeqNo;
-		sg_packet_wd.recvSeqNo = pack_info.recvSeqNo;
-		sg_packet_wd.indc_data = data_indc;
-		sg_packet_wd.magic_v2 = SG_MAGIC_VALUE;
-//		printf("\tsmagic: %d\n", 
-//			sg_packet_wd.magic_v2);
-		sg_packet_wd.data = (SG_Data_Block*)data;
-		*plen = sizeof(sg_packet_wd);//SG_DATA_PACKET_SIZE;
-//		printf("size: %ld\n", *plen);
-		sg_pckt_buff(packet, &sg_packet_wd, sizeof(sg_packet_wd));
-
-printf("serialize:\nmagic1: %d\nlocNodeId: %ld\nremNodeId: %ld\nblockID: %ld\noperation: %d\nsendSeqNo: %d\nrecvSeqNo: %d\nindc: %d\nmagic2: %d\n\n",
-    sg_packet_wd.magic_v1,
-    sg_packet_wd.locNodeId,
-    sg_packet_wd.remNodeId,
-    sg_packet_wd.blockID,
-    sg_packet_wd.operation,
-    sg_packet_wd.sendSeqNo,
-    sg_packet_wd.recvSeqNo,
-    sg_packet_wd.indc_data,
-    sg_packet_wd.magic_v2);
-	} else {
-		sg_packet_nd.magic_v1 = SG_MAGIC_VALUE;
-		sg_packet_nd.locNodeId = pack_info.locNodeId;
-		sg_packet_nd.remNodeId = pack_info.remNodeId;
-		sg_packet_nd.blockID = pack_info.blockID;
-		sg_packet_nd.operation = pack_info.operation;
-		sg_packet_nd.sendSeqNo = pack_info.sendSeqNo;
-		sg_packet_nd.recvSeqNo = pack_info.recvSeqNo;
-		sg_packet_nd.indc_data = data_indc;
-//		sg_packet_nd.data = NULL;
-		sg_packet_nd.magic_v2 = SG_MAGIC_VALUE;
-		*plen = sizeof(sg_packet_nd);//SG_BASE_PACKET_SIZE;
-//		printf("size: %ld\n", *plen);
-		sg_pckt_buff(packet, &sg_packet_nd, sizeof(sg_packet_nd));
-		
-//		printf("\tsmagic: %d\n", 
-//			sg_packet_nd.magic_v2);
-printf("serialize:\nmagic1: %d\nlocNodeId: %ld\nremNodeId: %ld\nblockID: %ld\noperation: %d\nsendSeqNo: %d\nrecvSeqNo: %d\nindc: %d\nmagic2: %d\n\n",
-    sg_packet_nd.magic_v1,
-    sg_packet_nd.locNodeId,
-    sg_packet_nd.remNodeId,
-    sg_packet_nd.blockID,
-    sg_packet_nd.operation,
-    sg_packet_nd.sendSeqNo,
-    sg_packet_nd.recvSeqNo,
-    sg_packet_nd.indc_data,
-    sg_packet_nd.magic_v2);
-
+		impose_buff(packet, (char*)data, 0, SG_BLOCK_SIZE - 1, packet_margin);
+		packet_margin += SG_BLOCK_SIZE;
 	}
+	//magic
+	impose_buff(packet, (char*)&magic_num, 0, sizeof(uint32_t) - 1, packet_margin);
+	
 	return(pack_status); 
 }
 
@@ -232,90 +171,97 @@ SG_Packet_Status deserialize_sg_packet( SG_Node_ID *loc, SG_Node_ID *rem, SG_Blo
         SG_System_OP *op, SG_SeqNum *sseq, SG_SeqNum *rseq, char *data, 
         char *packet, size_t plen )
 {
+	uint32_t packet_margin = 0;
+	//magic value
+	uint32_t magic_v1;
+	strcpy((char*)&magic_v1, packet);
+	packet_margin += sizeof(uint32_t);
+	//locNodeId
+	impose_buff((char*)loc, packet, packet_margin, packet_margin + sizeof(SG_Node_ID) - 1, 0);
+	packet_margin += sizeof(SG_Node_ID);
+	//remNodeId
+	impose_buff((char*)rem, packet, packet_margin, packet_margin + sizeof(SG_Node_ID) - 1, 0);
+	packet_margin += sizeof(SG_Node_ID);
+	//blockID
+	impose_buff((char*)blk, packet, packet_margin, packet_margin + sizeof(SG_Block_ID) - 1, 0);
+	packet_margin += sizeof(SG_Block_ID);
+	//operation
+	impose_buff((char*)op, packet, packet_margin, packet_margin + sizeof(SG_System_OP) - 1, 0);
+	packet_margin += sizeof(SG_System_OP);
+	//sendSeqNo
+	impose_buff((char*)sseq, packet, packet_margin, packet_margin + sizeof(SG_SeqNum) - 1, 0);
+	packet_margin += sizeof(SG_SeqNum);
+	//recvSeqNo	
+	impose_buff((char*)rseq, packet, packet_margin, packet_margin + sizeof(SG_SeqNum) - 1, 0);
+	packet_margin += sizeof(SG_SeqNum);
+	//indicator
+	char data_indc;
+	impose_buff(&data_indc, packet, packet_margin, packet_margin + sizeof(char) - 1, 0);
+	packet_margin += sizeof(char);
+	//data
+	if (data_indc) {
+		impose_buff(data, packet, packet_margin, packet_margin + SG_BLOCK_SIZE - 1, 0);
+		packet_margin += SG_BLOCK_SIZE;
+	} else
+		data = NULL;
+	uint32_t magic_v2;
+	impose_buff((char*)&magic_v2, packet, packet_margin, packet_margin + sizeof(uint32_t) - 1, 0);
+	packet_margin += sizeof(uint32_t);
+	
 	SG_Packet_Status pack_status;
 	pack_status = SG_PACKT_OK;
-	if (data == NULL)
-		printf ("data null!------------------\n");
-	char data_indc = -1;
-	void *sg_packet = (void*)packet;
-	char werr = 0;
-	if (sizeof(SG_Packet_WD) == plen)
-		data_indc = 1;
-	if (sizeof(SG_Packet_ND) == plen)
-		data_indc = 0;
-
-	if (data_indc == -1) {
-		if (!werr) {
+	if (!*loc) {
+		pack_status = SG_PACKT_LOCID_BAD;
+		return(pack_status);
+	}
+	//
+	if (!*rem) {
+		pack_status = SG_PACKT_REMID_BAD;
+		return(pack_status);
+	}
+	//
+	if (!*blk) {
+		pack_status = SG_PACKT_BLKID_BAD;
+		return(pack_status);
+	}
+	//
+	if (!(*op >= SG_INIT_ENDPOINT && *op <= SG_MAXVAL_OP)) {
+		pack_status = SG_PACKT_OPERN_BAD;
+		return(pack_status);
+	}
+	//
+	if (!*sseq) {
+		pack_status = SG_PACKT_SNDSQ_BAD;
+		return(pack_status);
+	}
+	//
+	if (!*rseq) {
+		pack_status = SG_PACKT_RCVSQ_BAD;
+		return(pack_status);
+	}
+	if (data_indc) {
+		if (!data) {
+			pack_status = SG_PACKT_BLKDT_BAD;
+			return(pack_status);
+		}
+	}
+	if (data_indc) {
+		if (packet_margin != SG_DATA_PACKET_SIZE) {
 			pack_status = SG_PACKT_BLKLN_BAD;
-			werr = 1;
-			return pack_status;
+			return(pack_status);
 		}
+	} else
+		if (packet_margin != SG_BASE_PACKET_SIZE) {
+			pack_status = SG_PACKT_BLKLN_BAD;
+			return(pack_status);
 	}
-	if (((SG_Packet_ND*)sg_packet)->locNodeId)
-		*loc = ((SG_Packet_ND*)sg_packet)->locNodeId;
-	else {
-		if (!werr) {
-			pack_status = SG_PACKT_LOCID_BAD;
-			werr = 1;
-		}
+	if (magic_v1 != SG_MAGIC_VALUE || magic_v2 != SG_MAGIC_VALUE) {
+		pack_status = SG_PACKT_PDATA_BAD;
+		return(pack_status);
 	}
-	if (((SG_Packet_ND*)sg_packet)->remNodeId)
-		*rem = ((SG_Packet_ND*)sg_packet)->remNodeId;
-	else
-		if (!werr) {
-			pack_status = SG_PACKT_REMID_BAD;
-			werr = 1;
-		}
-	if (((SG_Packet_ND*)sg_packet)->blockID)
-		*blk = ((SG_Packet_ND*)sg_packet)->blockID;
-	else
-		if (!werr) {
-			pack_status = SG_PACKT_BLKID_BAD;
-			werr = 1;
-		}
-	if (((SG_Packet_ND*)sg_packet)->operation >= SG_INIT_ENDPOINT
-			&& ((SG_Packet_ND*)sg_packet)->operation <= SG_MAXVAL_OP)
-		*op = ((SG_Packet_ND*)sg_packet)->operation;
-	else
-		if (!werr) {
-			pack_status = SG_PACKT_OPERN_BAD;
-			werr = 1;
-		}
-	if (((SG_Packet_ND*)sg_packet)->sendSeqNo)
-		*sseq = ((SG_Packet_ND*)sg_packet)->sendSeqNo;
-	else
-		if (!werr) {
-			pack_status = SG_PACKT_SNDSQ_BAD;
-			werr = 1;
-		}
-	if (((SG_Packet_ND*)sg_packet)->recvSeqNo)
-		*rseq = ((SG_Packet_ND*)sg_packet)->recvSeqNo;
-	else
-		if (!werr) {
-			pack_status = SG_PACKT_RCVSQ_BAD;
-			werr = 1;
-		}
-	if (data_indc == 1)
-		strcpy(data, (char*)(((SG_Packet_WD*)sg_packet)->data));
-	else
-		data = NULL;
-	printf("pack status: %d\n", pack_status);
-printf("deserialize:\nmagic1: %d\nlocNodeId: %ld\nremNodeId: %ld\nblockID: %ld\noperation: %d\nsendSeqNo: %d\nrecvSeqNo: %d\nindc: %d\nmagic2: %d\n\n",
-    ((SG_Packet_WD*)sg_packet)->magic_v1,
-    *loc,
-    *rem,
-    *blk,
-    *op,
-    *sseq,
-    *rseq,
-    data_indc,
-	((SG_Packet_WD*)sg_packet)->magic_v2);
-	
+
 	return(pack_status);
 }
-
-
-
 
 
 
